c093e429e9e01889f4c03abd736de27d737f9985
Ivan Santos <ivancsantos@hotmail.com.br>
2023-12-04 15:44:25 -0300

    pequenos fix

--- trabalho.js
+++ trabalho.js
@@ -14,14 +14,8 @@ const MYSQL_LOGIN="root";
 const MYSQL_PASSWORD="a1b2c3d4";
 const DATABASE = "employees"; // Usando modelo testdb https://github.com/datacharmer/test_db
 let con = mysql.createConnection({host: MYSQL_IP, user: MYSQL_LOGIN, password: MYSQL_PASSWORD, database: "employees"});
-con.connect(function(err) { if (err) throw err; console.log("Connection with mysql established");});
 
 // Cassandra https://github.com/rafaelqg/code/blob/main/cassandra_my_sql_integration.js
-//const cassandra  = require("cassandra-driver");
-// let authProvider = new cassandra.auth.PlainTextAuthProvider('cassandra', 'cassandra');
-// let contactPoints = ['localhost'];
-// let localDataCenter = 'DataCenter1';
-// let client = new cassandra.Client({contactPoints: contactPoints, authProvider: authProvider, localDataCenter: localDataCenter, keyspace:'employees'});
 let cassandra = require("cassandra-driver");
 const credentials_datastax = {
   "clientId": "yuQwSfDitkGDHUcEkLMEzDgP",
@@ -43,62 +37,14 @@ const client = new cassandra.Client({
   }
 });
 
-// async function migrateMySQLToCassandra() {
-//   // Keyspace management is currently only supported at https://astra.datastax.com/org/37a269a1-f0dc-4474-8128-59408fa9c61d/database/68d34aa9-647d-4d89-bc44-dc65dc668175
-//   // await client.connect(function(e) {
-//   //   let query = "CREATE KEYSPACE IF NOT EXISTS employees WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '3' }";
-//   //   return client.execute(query, function(e, res) {
-//   //     return console.log(e, res);
-//   //   });
-//   // });
-  
-//   console.log("Chegou no drop table if exists");
-//   await client.execute("DROP TABLE IF EXISTS employees.bymanager");
-//   await client.execute("CREATE TABLE employees.bymanager (manager text PRIMARY KEY, department text, employee text);");
-
-//   console.log("Chegou na instrução de montar a query SQL")
-
-//   let sql=`SELECT concat(concat(e.first_name," "), e.last_name ) as Manager,
-// 	      d.dept_name as Department,
-// 	      concat(concat(e2.first_name," "), e2.last_name ) as Employee
-//         FROM dept_manager m
-// 	      INNER JOIN employees e ON (m.emp_no = e.emp_no)
-// 	      INNER JOIN departments d ON (m.dept_no = d.dept_no)
-// 	      LEFT JOIN dept_emp de ON (d.dept_no = de.dept_no)
-// 	      LEFT JOIN employees e2 ON (de.emp_no = e2.emp_no)
-//         WHERE m.to_date = '9999-01-01'`;
-
-//   console.log("Chegou na query de insert");
-
-// //   con.query(sql, function (err, result) {
-// //     console.log("Entrou na função con.query");
-// //     let count = 0;
-// //     result.forEach(async record => {
-// //       console.log(count++, "..");
-// //       await client.connect();
-// //       let sql ="INSERT INTO employees.bymanager (manager, department, employee)";
-// //       sql+= ` VALUES('${record["Manager"]}','${record["Department"]}',${record["Employee"]}`;
-// //       await client.execute(sql);
-// //     });
-// // });
-//   //await con.connect(async function (err, result, fields) {
-//     //console.log(err, result, fields);
-//     //if (err) throw err;
-//     await con.query("select * from employees;", function(err, result, fields) {
-//       console.log("Entrou no con.query");
-//       if(err) throw err;
-//       console.log(result, fields);
-//     });
-//   //});
-// }
-
 let loadDataToDatastax = async function (){
+  con.connect(function(err) { if (err) throw err; console.log("Connection with mysql established");});
   await client.connect();
   
   // Questão 2a
   // await client.execute("DROP TABLE IF EXISTS employees.bymanager");
   // await client.execute("CREATE TABLE employees.bymanager (manager text, department text, employee text, PRIMARY KEY(manager, department, employee));");
-  // ////await client.execute("CREATE INDEX ON employees.bymanager (manager);");
+  
   // let sql=`SELECT 
   //           concat(concat(e.first_name," "), e.last_name ) as Manager,
   //           d.dept_name as Department,
@@ -111,25 +57,22 @@ let loadDataToDatastax = async function (){
   //         WHERE m.to_date = '9999-01-01';`;
 
   // await con.query(sql, async function(err, result, fields) {
-  //     if(err) throw err;
-  //     //console.log("Result: " + JSON.stringify(result));
-  //     //let count = 0;
-  //     await result.forEach(async record => {
-  //       //console.log(++count);
-  //       let sql ="INSERT INTO employees.bymanager (manager, department, employee)";
-  //       sql+= ` VALUES('${record["Manager"]}','${record["Department"]}','${record["Employee"]}');`;
-  //       //console.log(sql);
-  //       await client.connect();
-  //       const rs = await client.execute(sql);
-  //       console.log(`Your cluster returned ${rs.rowLength} row(s)`);
-  //     });
+  //   if(err) throw err;
+
+  //   await result.forEach(async record => {
+  //     let sql ="INSERT INTO employees.bymanager (manager, department, employee)";
+  //     sql+= ` VALUES('${record["Manager"]}','${record["Department"]}','${record["Employee"]}');`;
+  //     //console.log(sql);
+  //     const rs = await client.execute(sql);
+  //     console.log(`Your cluster returned ${rs.rowLength} row(s)`);
   //   });
-  //   // Fim Questão 2a
+  // });
+  // Fim Questão 2a
 
     // Questão 2b
   await client.execute("DROP TABLE IF EXISTS employees.byDepartmentAndDate");
   await client.execute("CREATE TABLE employees.byDepartmentAndDate (department text, from_date date, to_date date, employee text, PRIMARY KEY(department, from_date, to_date));");
-  //await client.execute("CREATE INDEX ON employees.bymanager (manager);");
+
   sql=`SELECT
 	          d.dept_name as Department,
 	          DATE_FORMAT(de.from_date, '%Y-%c-%d') as FromDate,
@@ -141,29 +84,43 @@ let loadDataToDatastax = async function (){
 
   await con.query(sql, async function(err, result, fields) {
       if(err) throw err;
-      //console.log("Result: " + JSON.stringify(result));
-      //let count = 0;
       await result.forEach(async record => {
-        //console.log(++count);
         let sql ="INSERT INTO employees.byDepartmentAndDate (department, from_date, to_date, employee)";
         sql+= ` VALUES('${record["Department"]}','${record["FromDate"]}','${record["ToDate"]}','${record["Employee"]}');`;
-        console.log(sql);
-        await client.connect();
+        //console.log(sql);
         const rs = await client.execute(sql);
         console.log(`Your cluster returned ${rs.rowLength} row(s)`);
       });
     });
     // Fim Questão 2b
     
-  //await client.shutdown();
 };
 
-loadDataToDatastax();
+// 2a
+let getEmployeesByManager = async function() {
+  let query = prompt("Informe o nome do manager que deseja pesquisar: ")
+  let sql = `SELECT * from employees.bymanager WHERE manager = '${query}';`;
+  console.log(sql);
+  const rs = await client.execute(sql);
+  console.log(rs.rows);
+}
+
+// 2b
+let getEmployeesByDepartmentAndDate = async function() {
+  let query = prompt("Informe o nome do departamento que deseja pesquisar: ")
+  let queryDate = prompt("Informe a data que deseja pesquisar ('YYYY-MM-DD'): ");
+  let sql = `SELECT * from employees.byDepartmentAndDate WHERE department = '${query}' AND from_date <= '${queryDate}';`;
+  console.log(sql);
+  const rs = await client.execute(sql);
+  console.log(rs.rows);
+}
+
+//loadDataToDatastax();
 
 const displayMenuOptions = function() {
   console.log("*** SELECIONE A OPÇÃO DESEJADA ***");
   console.log("1. Sincroniza dados do MySQL para o Cassandra");
-  console.log("2. Consulta funcionário a partir de id do manager");
+  console.log("2. Retorna todos os employeers a partir do manager");
   console.log("3. Consulta funcionário a partir de um title");
   console.log("4. Consulta funcionário a partir de um departamento");
   console.log("5. Relatório de média salarial de funcionários por departamento");
@@ -181,16 +138,14 @@ async function menu() {
       }
       switch(option) {
           case 1:
-              await connectMySQL();
+              loadDataToDatastax();
               break;
           case 2:
-              let promptManager = prompt("Qual gerente deseja consultar? ");
-              await getEmployeeByManager(promptManager);
-              break
+              getEmployeesByManager();
+              break;
           case 3:
-              let promptTitle = prompt("Qual title deseja consultar? ");
-              await getEmployeeByTitles(promptTitle);
-              break
+              await getEmployeesByDepartmentAndDate();
+              break;
           case 4:
               let promptDepartment = prompt("Qual departamento deseja consultar? ");
               await getEmployeeByDepartment(promptDepartment);
@@ -205,4 +160,4 @@ async function menu() {
       }
   }
 }
-//menu();
+menu();
